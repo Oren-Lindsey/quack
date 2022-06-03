ad561825a985f9d99288dd927759a7a16a0ff539
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-06-03 18:55:48 -0400

    fix labeling problem

--- src/routes/session/index.svelte
+++ src/routes/session/index.svelte
@@ -18,9 +18,9 @@
     <button class="transition ease-in-out delay-50 p-2 rounded-md inline-block bg-yellow-500 shadow-lg hover:shadow-2xl hover:bg-yellow-400 dark:hover:bg-slate-800" type="submit">Go</button>
 </form>
 <form on:submit|preventDefault={goToId} class="transition ease-in-out delay-50 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 hover:shadow-xl dark:bg-slate-900">
-    <label for="name" class="text-gray-500">Or, go to a session by id:</label>
+    <label for="id" class="text-gray-500">Or, go to a session by id:</label>
     <br>
-    <input class="transition ease-in-out delay-50 shadow appearance-none border rounded w-full py-2 px-3 my-4 text-gray-500 leading-tight focus:outline-none focus:shadow-outline hover:shadow-xl dark:bg-gray-900 dark:text-white" name="name" id="name" placeholder="id of session..." bind:value={id} />
+    <input class="transition ease-in-out delay-50 shadow appearance-none border rounded w-full py-2 px-3 my-4 text-gray-500 leading-tight focus:outline-none focus:shadow-outline hover:shadow-xl dark:bg-gray-900 dark:text-white" name="id" id="id" placeholder="id of session..." bind:value={id} />
     <br>
     <button class="transition ease-in-out delay-50 p-2 rounded-md inline-block bg-yellow-500 shadow-lg hover:shadow-2xl hover:bg-yellow-400 dark:hover:bg-slate-800" type="submit">Go</button>
 </form>
