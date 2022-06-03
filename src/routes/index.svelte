66a0631603b24751c6134ef6c58870ed5083e989
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-06-03 17:47:59 -0400

    rollback underline styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,7 +17,7 @@
 />
 <h1 class="text-black text-2xl dark:text-white">Quack</h1>
 <p class="text-gray-500 pt-2">
-    Quack is a <b class="delay-50 underline decoration-yellow-400 decoration-wavy decoration-1 dark:decoration-gray-500 hover:decoration-dotted">simple</b> and <b class="underline decoration-cyan-400 decoration-1 decoration-wavy dark:decoration-gray-500 hover:decoration-dotted">fast</b> way to share and answer questions. Get started by creating a new session.
+    Quack is a <b class="delay-50 underline decoration-yellow-400 decoration-wavy decoration-2 dark:decoration-gray-500 hover:decoration-dotted">simple</b> and <b class="underline decoration-cyan-400 decoration-2 decoration-wavy dark:decoration-gray-500 hover:decoration-dotted">fast</b> way to share and answer questions. Get started by creating a new session.
 </p>
 <br>
 <a href="/new" class="transition ease-in-out delay-50 p-2 rounded-md inline-block bg-yellow-500 shadow-lg hover:shadow-2xl hover:bg-yellow-400">New session</a>
